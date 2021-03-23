a4322cd82feb11836cd34778544b87e885e04a33
Aviv Shabo <33037088+avivshabo@users.noreply.github.com>
2021-03-23 21:54:22 +0200

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -4,27 +4,32 @@ import console
 from prettytable import PrettyTable
 
 #elements = console.get_int_array()
-#elements = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+elements = [1, 1, 3, 5, 7, 9, 22, 11, 4, 37, 5]
+
 maxElem = max(elements)
 minElem = min(elements)
-bucketsNum = console.get_int()
-#bucketsNum = 3
+#bucketsNum = console.get_int()
+bucketsNum = 3
 rangeSize = maxElem - minElem + 1  #12
 bucketSize =  rangeSize//bucketsNum #integer division   4
 
-print(f"Max element:{maxElem}\nMin element:{minElem}\nrange size:{rangeSize}\nbucket size:{bucketSize}\n\n")
+
+print(f"+-----------------------------+")
+print(f"Total elements: {len(elements)}\nMax element:{maxElem}\nMin element:{minElem}\nRange size:{rangeSize}\nBucket size:{bucketSize}")
+print(f"+-----------------------------+")
+print(f"\n\n")
 buckets = [0] * bucketsNum
 bucketElements =  [""] * bucketsNum
 
-histogramTable = PrettyTable(['Bucket', 'Count', 'Elements'])
+histogramTable = PrettyTable(['Bucket', 'Bucket Range', 'Count', 'Elements'])
 for index in range(bucketsNum):
     bucketMin = minElem + bucketSize*index
-    bucketMax = bucketMin + bucketSize - 1
+    bucketMax = bucketMin + bucketSize
     for element in elements:
         if bucketMin <= element <= bucketMax:
             buckets[index] += 1
-            bucketElements[index] += f"{element},"
-            
-    histogramTable.add_row([index, buckets[index], bucketElements[index]])
+            bucketElements[index] = ",".join(filter(None,[bucketElements[index],str(element)]))
+    histogramTable.add_row([index, f"{bucketMin}..{bucketMax}", buckets[index], bucketElements[index]])
+
 print("Histogram Table:")
 print(histogramTable)
